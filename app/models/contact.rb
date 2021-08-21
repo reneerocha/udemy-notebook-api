70bb1a501a595041d21f0c089abdb8568091cbdf
Renee Rocha <reneerocha.pi@gmail.com>
2021-08-21 10:33:07 -0300

    overwrite as_json include Kind

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,16 @@
 class Contact < ApplicationRecord
     belongs_to :kind
+
+    def author
+        "Renee"
+    end
+
+    def as_json(options={})
+        super(
+            root: true,
+            methods: :author, 
+            include: { kind: {only: :description}}
+            )
+    end
+
 end
